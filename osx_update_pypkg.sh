4c14de7c86caddaa0f34330bb6e1c4dcc59463fc
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-02-04 17:45:12 +0800

    edit osx_update_pypkg.sh 200204

--- osx_update_pypkg.sh
+++ osx_update_pypkg.sh
@@ -7,6 +7,7 @@ cd ~
 sudo -H pip3 install -U pip
 sudo -H pip3 install -U numpy
 sudo -H pip3 install -U scipy
+sudo -H pip3 install -U Pygments
 sudo -H pip3 install -U matplotlib
 sudo -H pip3 install -U requests-html
 sudo -H pip3 install -U jupyter notebook
