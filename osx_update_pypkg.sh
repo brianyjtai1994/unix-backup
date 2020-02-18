f67d13e44facd4802f44d5fbddbd4e617f331ded
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-02-18 22:27:18 +0800

    edit osx_update_pypkg.sh 200218

--- osx_update_pypkg.sh
+++ osx_update_pypkg.sh
@@ -12,5 +12,6 @@ sudo -H pip3 install -U matplotlib
 sudo -H pip3 install -U requests-html
 sudo -H pip3 install -U jupyter notebook
 sudo -H pip3 install -U opencc-python-reimplemented
+sudo -H pip3 install git+https://github.com/brianyjtai1994/teralab.git
 
 clear
