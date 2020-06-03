2ddb689fe01c573774894842e17570232e4749ed
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-06-03 17:40:44 +0800

    edit osx_update_pypkg.sh 200603: add PyQt5

--- osx_update_pypkg.sh
+++ osx_update_pypkg.sh
@@ -9,6 +9,7 @@ sudo -H pip3 install -U numpy
 sudo -H pip3 install -U scipy
 sudo -H pip3 install -U Pygments
 sudo -H pip3 install -U matplotlib
+sudo -H pip3 install -U PyQt5
 sudo -H pip3 install -U requests-html
 sudo -H pip3 install -U jupyter notebook
 sudo -H pip3 install -U opencc-python-reimplemented
