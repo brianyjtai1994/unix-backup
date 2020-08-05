126a67650ccbf7d77184ba5c745fe4ad97f68fb5
brianyjtai1994 <brianyjtai1994@gmail.com>
2020-08-06 01:00:41 +0800

    update OpenCC

--- osx_update_pypkg.sh
+++ osx_update_pypkg.sh
@@ -12,7 +12,7 @@ sudo -H pip3 install -U matplotlib
 sudo -H pip3 install -U PyQt5
 sudo -H pip3 install -U requests-html
 sudo -H pip3 install -U jupyter notebook
-sudo -H pip3 install -U opencc-python-reimplemented
+sudo -H pip3 install -U OpenCC
 sudo -H pip3 install git+https://github.com/brianyjtai1994/teralab.git@v0.1.3
 
 clear
