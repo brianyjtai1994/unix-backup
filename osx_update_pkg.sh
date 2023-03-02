b446681bbd4901a917f31a978d9679f6c5f1aeda
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-03-02 12:37:40 +0800

    init osx_update_pkg.sh

---
+++ osx_update_pkg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo clear
+cd ~
+
+brew update
+brew upgrade
+brew cleanup
+
+python3.11 -m pip install --upgrade pip
+python3.11 -m pip install --upgrade numpy
+python3.11 -m pip install --upgrade scipy
+python3.11 -m pip install --upgrade matplotlib
+python3.11 -m pip install --upgrade PyQt5
+python3.11 -m pip install --upgrade jupyter notebook
+
+sudo tlmgr update --all --reinstall-forcibly-removed
+
+brew upgrade --fetch-HEAD zig
